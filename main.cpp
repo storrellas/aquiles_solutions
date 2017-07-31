c3cbee5e422d9d98a512959c1b709dc9c2a7e257
storrellas <storrellas@gmail.com>
2017-07-31 18:57:17 +0200

    ENH: Preliminary version

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+const int BOARD_SIZE = 8;
+const int TILE = 1;
+const int EMPTY = 0;
+int board[8][8] = {
+    TILE, TILE,  TILE,  EMPTY, TILE,  TILE,  TILE,TILE,
+    TILE, EMPTY, TILE,  TILE,  TILE,  EMPTY, TILE, EMPTY,
+    TILE, EMPTY, EMPTY, TILE,  EMPTY, EMPTY, TILE, TILE,
+    TILE, TILE,  EMPTY, TILE,  EMPTY, EMPTY, EMPTY, TILE,
+    TILE, EMPTY, EMPTY, TILE,  TILE,  TILE,  TILE, TILE,
+    TILE, TILE,  TILE,  TILE,  EMPTY, TILE,  EMPTY, EMPTY,
+    TILE, EMPTY, EMPTY, TILE,  EMPTY, TILE,  TILE,  TILE,
+    TILE, TILE,  EMPTY, TILE,  TILE,  TILE,  EMPTY, TILE,
+
+};
+
+/**
+ * Prints the current matrix status
+ */
+void print_matrix(){
+
+    for(int i = 0; i < BOARD_SIZE; ++i){
+        for(int j = 0; j < BOARD_SIZE; ++j){
+            if( board[i][j] == TILE ) cout << "X";
+            else cout << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    print_matrix();
+    return 0;
+}
+
